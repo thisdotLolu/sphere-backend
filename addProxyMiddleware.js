80645cfa868d8c4fa093cd5e425fb9361cd16e4b
Lu <66273292+thisdotLolu@users.noreply.github.com>
2023-10-16 20:55:26 +0100

    Add files via upload

---
+++ addProxyMiddleware.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function addProxyMiddleware(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://api.1inch.dev/swap/v5.2/1/swap',
+      changeOrigin: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
+}),);
+};
